e8020a019c39f1f5c2de140e9bb9327a013b73e9
rahimuj570 <rahimuj570@gmail.com>
2022-02-10 17:39:25 +0600

    google font added and minor bug fix

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 let display = document.querySelector("#screen");
 const allBtn = document.querySelectorAll("button");
-if (display.value === "") {
-  document.getElementById("answer").disabled = true;
-}
+
 for (const btn of allBtn) {
   btn.addEventListener("click", function (e) {
     let btnText = e.target.innerText;
     if (btnText === "AC") {
       display.value = "";
     } else if (btnText === "÷") {
-      display.value = "/";
+      display.value += "/";
     } else if (btnText === "×") {
-      display.value = "*";
+      display.value += "*";
     } else if (btnText === "=") {
       display.value = eval(display.value);
     } else if (btnText === "√") {
